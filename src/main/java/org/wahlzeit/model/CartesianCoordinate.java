422f4e1a5b266422c185fa1e8dce9926e56eda22
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-11-13 17:25:07 +0100

    Use Math.toDegrees in CartesianCoordinate

--- src/main/java/org/wahlzeit/model/CartesianCoordinate.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinate.java
@@ -36,8 +36,8 @@ public class CartesianCoordinate implements Coordinate {
 
     @Override
     public SphericalCoordinate asSphericalCoordinate() {
-        final double latitude = 90 -Math.atan(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / z) * 180 / Math.PI;
-        final double longitude = Math.atan(y / x) * 180 / Math.PI;
+        final double latitude = 90 - Math.toDegrees(Math.atan(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / z));
+        final double longitude = Math.toDegrees(Math.atan(y / x));
         final double radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
         //TODO
         return new SphericalCoordinate(latitude, longitude, radius);
