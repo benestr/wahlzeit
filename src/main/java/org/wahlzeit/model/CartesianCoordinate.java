8f986f6dbc276ad43c144c934321ba5ff83be19c
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-11-13 17:25:37 +0100

    Remove obsolete TODO

--- src/main/java/org/wahlzeit/model/CartesianCoordinate.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinate.java
@@ -39,7 +39,7 @@ public class CartesianCoordinate implements Coordinate {
         final double latitude = 90 - Math.toDegrees(Math.atan(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / z));
         final double longitude = Math.toDegrees(Math.atan(y / x));
         final double radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
-        //TODO
+
         return new SphericalCoordinate(latitude, longitude, radius);
     }
 
