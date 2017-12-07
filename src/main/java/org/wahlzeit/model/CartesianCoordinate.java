0f93a887d9c0774e72458e4bf739a47a063558dc
Benedikt Strassner <benedikt.strassner@yahoo.de>
2017-12-07 13:48:49 +0100

    Extend error handling and contracts in CartesianCoordinate

--- src/main/java/org/wahlzeit/model/CartesianCoordinate.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinate.java
@@ -27,36 +27,66 @@ public class CartesianCoordinate extends AbstractCoordinate {
 
     @Override
     public CartesianCoordinate asCartesianCoordinate() {
-        return this;
+        checkClassInvariants();
+
+        final CartesianCoordinate result = this;
+
+        checkClassInvariants();
+        return result;
     }
 
     @Override
     public SphericalCoordinate asSphericalCoordinate() {
+        checkClassInvariants();
+
         final double latitude = 90 - Math.toDegrees(Math.atan(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / z));
         final double longitude = Math.toDegrees(Math.atan(y / x));
         final double radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
 
-        return new SphericalCoordinate(latitude, longitude, radius);
+        final SphericalCoordinate result = new SphericalCoordinate(latitude, longitude, radius);
+
+        checkClassInvariants();
+        return result;
     }
 
     double doGetCartesianDistance(CartesianCoordinate cartesianCoordinate) {
-        return Math.sqrt(
+        checkClassInvariants();
+
+        final double result = Math.sqrt(
                 Math.pow(x - cartesianCoordinate.x, 2) + Math.pow(y - cartesianCoordinate.y, 2) + Math.pow(z - cartesianCoordinate.z, 2));
+
+        checkClassInvariants();
+        return result;
     }
 
     @Override
     public boolean isEqual(Coordinate coordinate) {
+        checkClassInvariants();
         assertNotNull(coordinate, "Coordinate must not be null");
 
         if(!(coordinate instanceof CartesianCoordinate)) {
             return false;
         }
         final CartesianCoordinate cartesianCoordinate = (CartesianCoordinate) coordinate;
-        return (cartesianCoordinate.x == this.x) && (cartesianCoordinate.y == this.y) && (cartesianCoordinate.z == this.z);
+        final boolean result = (cartesianCoordinate.x == this.x) && (cartesianCoordinate.y == this.y) && (cartesianCoordinate.z == this.z);
+
+        checkClassInvariants();
+        return result;
     }
 
     @Override
     public boolean equals(Object object) {
-        return object instanceof CartesianCoordinate && isEqual((CartesianCoordinate) object);
+        checkClassInvariants();
+
+        final boolean result = object instanceof CartesianCoordinate && isEqual((CartesianCoordinate) object);
+
+        checkClassInvariants();
+        return result;
+    }
+
+    private void checkClassInvariants() {
+        assert x != Double.NEGATIVE_INFINITY && x != Double.POSITIVE_INFINITY && x != Double.NaN ;
+        assert y != Double.NEGATIVE_INFINITY && y != Double.POSITIVE_INFINITY && y != Double.NaN ;
+        assert z != Double.NEGATIVE_INFINITY && z != Double.POSITIVE_INFINITY && z != Double.NaN ;
     }
 }
