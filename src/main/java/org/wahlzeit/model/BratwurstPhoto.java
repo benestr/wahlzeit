52f74a4194182a3e2a870d036f9990722f07c995
Benedikt Strassner <benedikt.strassner@yahoo.de>
2018-01-19 10:31:47 +0100

    Add documentation for BratwurstPhoto creation

--- src/main/java/org/wahlzeit/model/BratwurstPhoto.java
+++ src/main/java/org/wahlzeit/model/BratwurstPhoto.java
@@ -3,6 +3,27 @@ package org.wahlzeit.model;
 import com.googlecode.objectify.annotation.Subclass;
 import org.wahlzeit.utils.PatternInstance;
 
+/**
+ *  <h3>How a BratwurstPhoto is created:</h3>
+ *  <ol>
+ *  	<li>To create a BratwurstPhoto, first the client has to call one of the createPhoto() methods of the {@link BratwurstPhotoManager} which is a Singleton and a subclass of {@link PhotoManager}
+ *  	Optionally a {@link Bratwurst} object can be passed. See the documentation of that class for a description of how those are created.</li>
+ *  	<li>The {@link BratwurstPhotoManager} than calls one of the static createBratwurstPhoto methods in {@link PhotoUtil} which forwards the creation request and configures some fields on the created object afterwards.</li>
+ *  	<li>PhotoUtils calls one of the createPhoto() methods of {@link BratwurstPhotoFactory} which is a singleton subclass of {@link PhotoFactory}</li>
+ *  	<li>BratwurstPhotoFactory then calls the actual constructors of {@link BratwurstPhoto}, creating and then returning the object</li>
+ *  	<li>The {@link BratwurstPhoto} either receives a {@link Bratwurst} object to associate with the Photo or, if not, uses a defaultBratwurst object of a defaultBratwurstType</li>
+ *  </ol>
+ *
+ *  <h3>The creation process in the solution space:</h3>
+ *  <ul>
+ *      <li>Delegation: seperate-object</li>
+ *      <li>Selection: sub-classing</li>
+ *      <li>Configuration: in-code</li>
+ *      <li>Instantiation: in-code</li>
+ *      <li>Initialization: in-second-step</li>
+ *      <li>Building: default</li>
+ *  </ul>
+ */
 @PatternInstance(
         patternName = "Factory",
         participants = {
